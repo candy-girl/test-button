a45f94a2dc583d0aef66ce03e26f3202154b001c
曾艳 <zengyan2@nnuo.com>
2021-01-10 14:41:13 +0800

    zy

--- .history/src/index_20210105215918.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.css';
-
-export default function NewButton(props) {
-  return (
-  <button style={{ color: 'red' }} className="btn-style">{props.name}</button>
-  )
-}
